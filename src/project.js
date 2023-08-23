2a2ce4f4b773831c3321ba85e44d9261938fe1e8
bernyig <bernardommguerreiro@gmail.com>
2023-08-23 21:14:14 +0200

    localStorage projects/ Select different project by ID functional

--- src/project.js
+++ src/project.js
@@ -1,12 +1,29 @@
+const projectContainer = document.querySelector("[data-projects]");
 const projectForm = document.querySelector("[data-new-project-form");
+const projectInput = document.querySelector("[data-new-project-input]");
+
+const LOCAL_STORAGE_PROJECT_KEY = "task.projects";
+const LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY = "task.selectedProjectId";
+let projects =
+  JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || [];
+let selectedProjectId = localStorage.getItem(
+  LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY
+);
+
+projectContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "li") {
+    selectedProjectId = e.target.dataset.projectId;
+    saveAndDisplay();
+  }
+});
 
 projectForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  const projectInput = document.querySelector("[data-new-project-input]").value;
-  if (projectInput == null || projectInput === " ") return;
-  const project = createProject(projectInput);
+  const projectName = projectInput.value;
+  if (projectName == null || projectName === " ") return;
+  const project = createProject(projectName);
   projects.push(project);
-  display();
+  saveAndDisplay();
   projectForm.reset();
 });
 
@@ -16,4 +33,38 @@ function createProject(name) {
     name: name,
     tasks: [],
   };
+}
+
+export function display() {
+  clearElement(projectContainer);
+  projects.forEach((project) => {
+    const projectElement = document.createElement("li");
+    projectElement.dataset.projectId = project.id;
+    projectElement.classList.add("defaultProject");
+    projectElement.innerHTML = project.name;
+    if (project.id === selectedProjectId) {
+      projectElement.classList.add("active-project");
+    }
+
+    projectContainer.appendChild(projectElement);
+  });
+}
+
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+export function save() {
+  localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projects));
+  localStorage.setItem(
+    LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY,
+    selectedProjectId
+  );
+}
+
+function saveAndDisplay() {
+  save();
+  display();
 }
